f0272baa179fb8909077e26829746894b67d33e8
Baba-yara <fababa@iu.edu>
2025-06-21 17:51:41 -0400

    Add gitignore and expand KFAC package

--- bsde_seed/kfac/__init__.py
+++ bsde_seed/kfac/__init__.py
@@ -1,5 +1,6 @@
 """KFAC utilities for physics-informed neural networks."""
 
 from .solver import KFACPINNSolver
+from .optimizer import kfac_update, _init_state
 
-__all__ = ["KFACPINNSolver"]
+__all__ = ["KFACPINNSolver", "kfac_update", "_init_state"]
