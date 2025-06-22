c503ba931ffed9530849321d75e3572678225e29
Baba-yara <fababa@iu.edu>
2025-06-22 01:20:28 -0400

    Build docs with notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,15 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'BSDE DSGE'
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx_autodoc_typehints']
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+    'nbsphinx',
+]
+
+# Execute notebooks during the build so CI fails on execution errors
+nbsphinx_execute = 'always'
 source_suffix = {'.md': 'markdown'}
 master_doc = 'index'
 html_theme = 'alabaster'
