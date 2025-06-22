ab4161d6868d029018f9c22629b328b9c20d81a0
Baba-yara <fababa@iu.edu>
2025-06-21 20:09:30 -0400

    docs: document KFAC PINN solver

--- bsde_dsgE/kfac/__init__.py
+++ bsde_dsgE/kfac/__init__.py
@@ -1,6 +1,6 @@
 """KFAC utilities for physics-informed neural networks."""
 
-from .optimizer import kfac_update
+from .optimizer import kfac_update, _init_state
 from .solver import KFACPINNSolver
 
-__all__ = ["KFACPINNSolver", "kfac_update"]
+__all__ = ["KFACPINNSolver", "kfac_update", "_init_state"]
